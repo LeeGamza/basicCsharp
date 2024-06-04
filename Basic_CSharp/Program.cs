3d7d244b23afeee78ee46ff830fb0319f31834db
LeeGamza <135112596+LeeGamza@users.noreply.github.com>
2024-06-04 15:12:47 +0900

    배열 연습문제중

--- Basic_CSharp/Program.cs
+++ Basic_CSharp/Program.cs
@@ -5,15 +5,30 @@ namespace Basic_CSharp
     
     class Program
     {
+        static int GetHighestScore(int[] scores)
+        {
+            return 0;
+        }
 
+        static int GetAverageScore(int[] scores)
+        {
+            return 0;
+        }
+
+        static int GetIndexOf(int[] scores, int value)
+        {
+            return -1;
+        }
+
+        static void Sort(int[] scores)
+        {
+            
+        }
         static void Main(string[] args)
         {
-            Game game = new Game();
-
-            while (true)
-            {
-                game.Process();
-            }
+            // 배열
+            int[] scores = new int[5] { 10, 30, 40, 20, 50 };
+            
         }
     }
 }
