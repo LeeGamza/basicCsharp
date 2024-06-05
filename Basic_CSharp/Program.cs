3cdcafddb40e196ddcc6944a30db3c4e330543b2
LeeGamza <135112596+LeeGamza@users.noreply.github.com>
2024-06-05 13:15:29 +0900

    Array Quiz

--- Basic_CSharp/Program.cs
+++ Basic_CSharp/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net.WebSockets;
+using System.Security.Cryptography;
+using Microsoft.Win32.SafeHandles;
 
 namespace Basic_CSharp
 {
@@ -7,28 +10,36 @@ namespace Basic_CSharp
     {
         static int GetHighestScore(int[] scores)
         {
-            return 0;
+
+            int score = scores.Max();
+            return score;
         }
 
         static int GetAverageScore(int[] scores)
         {
-            return 0;
+            return (int)scores.Average();
         }
 
         static int GetIndexOf(int[] scores, int value)
         {
-            return -1;
+            return Array.IndexOf(scores, value);
         }
 
         static void Sort(int[] scores)
         {
+            Array.Sort(scores);
             
         }
         static void Main(string[] args)
         {
             // 배열
-            int[] scores = new int[5] { 10, 30, 40, 20, 50 };
             
+            int[] scores = new int[5] { 10, 30, 40, 20, 50 };
+            Console.WriteLine($"Max : {GetHighestScore(scores)}");
+            Console.WriteLine($"Avr : {GetAverageScore(scores)}");
+            Console.WriteLine($"GetIndexOf : scores의 {GetIndexOf(scores, 100)}번째 인덱스 ** -1값은 잘못된 값을 넣을 시 반환됩니다. **");
+            Sort(scores);
+            Console.WriteLine($"Sort : {String.Join(", ", scores)}");
         }
     }
 }
