7420256bf54628be3712f7e721dffeb8e6366084
LeeGamza <135112596+LeeGamza@users.noreply.github.com>
2024-06-08 15:24:36 +0900

    11
    11

--- Basic_CSharp/Program.cs
+++ Basic_CSharp/Program.cs
@@ -5,46 +5,23 @@ using Microsoft.Win32.SafeHandles;
 
 namespace Basic_CSharp
 {
+    // Observer Pattern
     class Program
     {
-        abstract class Monster      // 추상클래스
+        static void OnInputTest()
         {
-            public abstract void Shout(); // 추상함수
+            Console.WriteLine("Input Received!");
         }
-
-        interface IFlayable         // 나의 기능을 갖고 있는 아이라면 반드시 Fly 함수를 제공해야한다
-        {
-            void Fly();
-        }
-
-        class Orc : Monster
-        {
-            public override void Shout()    // 추상클래스안에있는 추상함수를 반드시 사용하라 라고 강요함
-            {
-                Console.WriteLine("록타르 오가르!");
-            }
-        }
-
-        class FlableOrc : Orc, IFlayable      // C#에서는 다중상속 안됨!
-        {
-            public void Fly()
-            {
-                
-            }
-        }
-
-        class Skeleton : Monster
-        {
-            public override void Shout()
-            {
-                Console.WriteLine("꾸에에에엑");
-            }
-        }
-        
         static void Main(string[] args)
         {
-            // 추상클래스와 Interface
+            InputManager inputManager = new InputManager();
+
+            inputManager.InputKey += OnInputTest;
             
+            while (true)
+            {
+                inputManager.Update();
+            }
         }
     }
 }
